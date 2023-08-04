87220487b6077771808828c7543231602bd71490
Iamneo <techadmin@iamneo.ai>
2023-08-04 10:15:46 +0000

    Backup Commit

---
+++ angularapp/src/app/interface/food.ts
@@ -0,0 +1,6 @@
+export class Food {
+    name: string | undefined;
+    calories: number | undefined;
+    image: string | undefined;
+    count: number | undefined;
+  }
---
+++ angularapp/src/app/today/today.component.ts
@@ -0,0 +1,36 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Food } from './../interface/food';
+import { items } from './../interface/foods';
+
+@Component({
+  selector: 'app-today',
+  templateUrl: './today.component.html',
+  styleUrls: ['./today.component.css']
+})
+export class TodayComponent implements OnInit {
+  items: Food[] = [];
+  newFood: any = {};
+  @Input()
+  totalCalories!: number;
+  constructor() {
+    
+  }
+
+  ngOnInit(): void {
+    this.items = items;
+  }
+
+  addCount(count: any, item: { count: any; calories: any; }): any {
+    item.count += count;
+    this.totalCalories += Number(count) * Number(item.calories)
+  }
+  // totalCalorie(){
+  //   for (let i=0;i<this.items.length;i++){
+  //     this.totalCalories += Number(this.items[i].calories)*Number(this.items[i].count);
+  //   }
+  //   return this.totalCalories;
+  // }
+
+
+
+}
